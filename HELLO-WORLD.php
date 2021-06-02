03aef8d75dc149632ea7ed99b6b786b58879c225
Rafael Antunes <rafao.lima@hotmail.com>
2021-06-02 00:15:35 -0300

    Add files via upload
    Hello-World em PHP

---
+++ HELLO-WORLD.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="_css/estilo.css";>
+	<title></title>
+</head>
+<body>
+<div>	
+	<?php
+		echo "Hello-World!";
+	?>
+</div>
+</body>
+</html>
